b9768a5b44ecfcbed5563f029025719f98a30a8d
DenisTunyk <tundenis43@gmail.com>
2023-04-06 18:05:36 +0300

    pause

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node_homework_cli
+
+1. https://monosnap.com/file/USvvUg96q7RoeUie2hKt8p2d4TnIIX (node index.js --action list)
+
+2. node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+
+https://monosnap.com/file/DlIOTtxT5xqmHZeeMcqwD2qqkpDVJy
+
+3. node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
--- contacts.js
+++ contacts.js
@@ -1,2 +1,48 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+}
+
+function updateContacts(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts), "utf8");
+}
+
+function getAllContacts() {
+  return listContacts();
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+
+  const contact = contacts.find((contact) => contact.id === id);
+
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const newContact = { id: nanoid(8), name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const newContacts = contacts.filter((value) => value.id !== id);
+  updateContacts(newContacts);
+  return newContacts;
+}
+
+module.exports = {
+  getAllContacts,
+  addContact,
+  getContactById,
+  removeContact,
+};
